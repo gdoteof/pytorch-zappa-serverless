e34e39a15a578c2e8642f43a336a0ef9e5a0c264
Geoff Golder <geoffgolder@gmail.com>
2018-11-29 15:23:44 -0500

    possibly download s3, use scratch space

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ from pytorch_models import awd_lstm
 import numpy as np
 import torch 
 import boto3
+import os.path
+
 
 app = Flask(__name__)
 CORS(app)
 
-MODEL_PATH= 'models/rjokes/rjokes.model.pth'
-ITOS_PATH = 'models/rjokes/rjokes.itos.pkl'
+S3_MODEL_PATH= 'rjokes/rjokes.model.pth'
+S3_ITOS_PATH = 'rjokes/rjokes.itos.pkl'
 
 def pickle_obj(data, path):
     with open(path,'wb+') as f:
@@ -27,16 +29,13 @@ def unpickle_obj(path):
         data = pickle.load(f)
     return data
 
-def unpickle_s3_obj(bucket, path):
-    if IS_LOCAL == False:
+def maybe_fetch_s3(bucket, path):
+    filename=f'/tmp/{path}'
+    if not os.path.exists(filename):
         s3 = boto3.resource('s3')
-        obj = s3.Object(bucket, path)
-        body_string = obj.get()['Body'].read()
-        data = pickle.loads(body_string)
-    else:
-        print('is local: ', IS_LOCAL)
-        data = unpickle_obj(path)
-    return data
+        s3.Bucket(bucket).download_file(path, filename)
+
+    return filename
     
 def sample_model(model, input_words, l=50):
     '''
@@ -86,18 +85,22 @@ def load_lm_and_predict():
     ''' input_data: data to be used in the prediction
     model_path: path to the 
     '''
+    MODEL_BUCKET=os.environ["models_bucket"]
     #Unpickles list representing vocabulary
-    itos = unpickle_obj(ITOS_PATH)
+    local_itos_path = maybe_fetch_s3(MODEL_BUCKET, S3_ITOS_PATH)
+    itos = unpickle_obj(local_itos_path)
     # Generates dictionary mapping token to int
     stoi = {i[1]:i[0] for i in enumerate(itos)}
     # Generates AWD_LSTM model
     dps = np.array([0.25, 0.1, 0.2, 0.02, 0.15]) * 1.0
     my_model = awd_lstm.get_language_model(vocab_sz=len(itos), emb_sz=1000, n_hid=1150, n_layers=3, pad_token=1, input_p=dps[0],                    output_p=dps[1],weight_p=dps[2], embed_p=dps[3], hidden_p=dps[4], tie_weights=True, bias=True, qrnn=False)
     # load all the weights in the model
-    my_model.load_state_dict(torch.load(MODEL_PATH, map_location='cpu'))
+    local_pth_path = maybe_fetch_s3(MODEL_BUCKET, S3_MODEL_PATH)
+    my_model.load_state_dict(torch.load(local_pth_path, map_location='cpu'))
     my_model.itos = itos
     my_model.stoi = stoi
     #print(my_model)
+    pickle_obj(my_model, 'rjokes.pkl')
     return {'text': sample_model(my_model, [''], l=200)}
 
 @app.route('/inference',methods=['GET'])
@@ -111,13 +114,31 @@ def inference():
 
 
 IS_LOCAL = False
+
+print("BOOTSTRAPPING")
+newpath = '/tmp/rjokes'
+print("CHECKING PATH")
+if not os.path.exists(newpath):
+        print(f'Path:{newpath} does not exist, creating it')
+        os.makedirs(newpath)
+print("WRITING TESTING FILE")
+with open(f'{newpath}/testing.txt', "w") as text_file:
+        text_file.write("IT WORKED")
+
 if __name__ == '__main__':
-    IS_LOCAL = True
+    print("BOOTSTRAPPING")
+    newpath = '/tmp/rjokes'
+    print("CHECKING PATH")
+    if not os.path.exists(newpath):
+            print(f'Path:{newpath} does not exist, creating it')
+            os.makedirs(newpath)
+    print("WRITING TESTING FILE")
+    with open(f'{newpath}/testing.txt', "w") as text_file:
+            text_file.write("IT WORKED")
     env = 'dev'
     # Test if the values were set to know if it is running locally or on lambda
     json_data = open('zappa_settings.json')
     env_vars = json.load(json_data)
     for key, val in env_vars[env]['aws_environment_variables'].items():
         os.environ[key] = val
-    print('Set the environ')
     app.run(debug=True, host='0.0.0.0', port=8082)
